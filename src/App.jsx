fc3041140fd32941e3bb3ce37406a94206effc29
KevinDonovan2 <hei.donovan.2@gmail.com>
2024-07-14 12:57:59 +0300

    chore: add dashboard on project

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import * as React from 'react';
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './users';
+import MyChart from './components/Chart';
+
+const Dashboard = () => (
+    <div>
+      <h1>Dashboard</h1>
+      <MyChart />
+    </div>
+  );
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
 const App = () => (
-    <Admin dataProvider={dataProvider}>
+    <Admin dataProvider={dataProvider} dashboard={Dashboard}>
         <Resource name="users" list={UserList} />
     </Admin>
 );
