c04f813f0a96696ddb8fdb454233e0ed8f4edc52
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2024-07-18 08:09:22 +0200

    feat: "Add Prtrimoine input  form"

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './components/users/users';
 import MyChart from './components/Chart';
+import { PatrimoineInput } from './components/patrimoine/patrimoine';
 
 const Dashboard = () => (
     <div>
@@ -16,6 +17,7 @@ const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
     <Admin dataProvider={dataProvider} dashboard={Dashboard}>
         <Resource name="users" list={UserList} />
+        <Resource name="Patrimoine" list={PatrimoineInput} />
     </Admin>
 );
 
