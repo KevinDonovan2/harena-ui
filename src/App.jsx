dba349cf0d46aee0201cf1c04e7016707a7aa3ce
KevinDonovan2 <hei.donovan.2@gmail.com>
2024-07-18 19:19:34 +0300

    fix: possession

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import MyChart from './components/Chart';
 import { PatrimoineInput } from './components/patrimoine/patrimoine';
-import  Possession  from './components/possession/possession';
 import UserIcon from '@mui/icons-material/Group';
 import InventoryIcon from '@mui/icons-material/Inventory';
+import { PossessionList } from './components/possession/possessionList';
+import { PossessionCreate } from './components/possession/possessionCreate';
+import { PossessionEdit } from './components/possession/possessionEdit';
 
 const Dashboard = () => (
     <div>
@@ -19,7 +21,7 @@ const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
     <Admin dataProvider={dataProvider} dashboard={Dashboard}>
         <Resource name="Patrimoine" list={PatrimoineInput} icon={UserIcon}/>
-        <Resource name="possession" list={Possession.list} create={Possession.create} edit={Possession.edit} icon={InventoryIcon}/>
+        <Resource name="possession" list={PossessionList} create={PossessionCreate} edit={PossessionEdit} icon={InventoryIcon}/>
     </Admin>
 );
 
